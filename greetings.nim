720b602f68ff770f3045048d9526c44d30af6fa2
Arialdo Martini <arialdomartini@gmail.com>
2015-04-20 22:01:09 +0200

    strings and raw strings

--- greetings.nim
+++ greetings.nim
@@ -2,6 +2,7 @@
 echo("What's your name?")
 var name = readLine(stdin)
 echo("Hi, ", name, "!")
-
+echo(r"There are also raw strings, as you see: c:\Windows\System32 \n.")
+echo("Without the r\"...\" escaped chars like \\n would produce this\nyou see?")
 
     
