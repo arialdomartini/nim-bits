252622716e418e81e71721df98a1ef91a476944e
Arialdo Martini <arialdomartini@gmail.com>
2015-04-20 22:55:32 +0200

    "when" is like "if" but it's evaluated at compile time

--- variables.nim
+++ variables.nim
@@ -1,39 +1,45 @@
-echo("Let's play with scopes")
+const compile = true
 
-block one:
-    const a = 1
-    echo("the constant a is ", $a)
+when compile == true:
+    echo("Let's play with scopes")
 
-#echo($a) would rise an error
+    block one:
+        const a = 1
+        echo("the constant a is ", $a)
 
-
-block two:
-    const a = 2
-    echo("I've been able to define a new constant, in another block. Its value is ", $a)
+    #echo($a) would rise an error
 
 
-var
-    a, b = 10
-    c = "text"
+    block two:
+        const a = 2
+        echo("I've been able to define a new constant, in another block. Its value is ", $a)
 
-echo "a = ", a
-echo "b = ", b
-echo "c = ", c
 
-b = 100
-echo "b = ", b
+    var
+        a, b = 10
+        c = "text"
 
-c = "another text"
-echo "c = ", c
+    echo "a = ", a
+    echo "b = ", b
+    echo "c = ", c
 
-echo "Once c is a string, its type cannot be changed"
-echo "c = 11 would rise an error"
+    b = 100
+    echo "b = ", b
 
-# both lk and res are constant, but lk must be defined at compile time
-const lk = 100
-let res = readLine(stdin)
+    c = "another text"
+    echo "c = ", c
 
-if res == "secret":
-    echo "Great, you discoverd the secret word"
+    echo "Once c is a string, its type cannot be changed"
+    echo "c = 11 would rise an error"
+
+    # both lk and res are constant, but lk must be defined at compile time
+    const lk = 100
+    let res = readLine(stdin)
+
+    if res == "secret":
+        echo "Great, you discoverd the secret word"
+    else:
+        echo "wrong, try with \"secret\""
+
 else:
-    echo "wrong, try with \"secret\""
+    echo "Not compiling"
