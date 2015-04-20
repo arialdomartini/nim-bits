f90dfdb8f628e396326c63470e5a0f7388ec9129
Arialdo Martini <arialdomartini@gmail.com>
2015-04-20 22:40:02 +0200

    while cycle and procedures

--- greetings.nim
+++ greetings.nim
@@ -1,6 +1,14 @@
 # this is a comment
-echo("What's your name?")
-var name = readLine(stdin)
+
+proc greets_and_ask : string =
+    echo("What's your name?")
+    return readLine(stdin)
+
+var name = greets_and_ask()
+while name != "foo":
+    echo("I'm sorry, you are not my friend \"foo\"")
+    name = greets_and_ask()
+    
 echo("Hi, ", name, "!")
 echo(r"There are also raw strings, as you see: c:\Windows\System32 \n.")
 echo("Without the r\"...\" escaped chars like \\n would produce this\nyou see?")
