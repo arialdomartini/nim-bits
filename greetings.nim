de98e28d018ad14b28f7ef41fc75a243d88e9aa4
Arialdo Martini <arialdomartini@gmail.com>
2015-04-20 23:05:10 +0200

    case can accept procedures

--- greetings.nim
+++ greetings.nim
@@ -4,12 +4,10 @@ proc greets_and_ask : string =
     echo("What's your name?")
     return readLine(stdin)
 
-var name = greets_and_ask()
-while name != "foo":
+while greets_and_ask() != "foo":
     echo("I'm sorry, you are not my friend \"foo\"")
-    name = greets_and_ask()
     
-echo("Hi, ", name, "!")
+echo("Hi, foo!")
 echo(r"There are also raw strings, as you see: c:\Windows\System32 \n.")
 echo("Without the r\"...\" escaped chars like \\n would produce this\nyou see?")
 
