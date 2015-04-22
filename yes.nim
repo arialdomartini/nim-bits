2eb33e23da507f1e3cf8ad16ebc0d81f2c63b3df
Arialdo Martini <arialdomartini@gmail.com>
2015-04-22 06:38:02 +0200

    passing a variable to a procedure

--- yes.nim
+++ yes.nim
@@ -1,14 +1,16 @@
-proc yes(question: string) : bool = # return type is mandatory
-    var question = if question == "classical" : "do you like nim?" else: question
-    echo question
+proc yes(question: string, questionmark: var string) : bool = # return type is mandatory
+    questionmark = "??"
+    var question = if question == "classical" : "do you like nim" else: question
+    echo question, questionmark
     while true:
         case readLine(stdin):
             of "y", "Y", "yes", "Yes": return true
             of "n", "N", "no", "No": return false
             else:
-                echo "Plese, reply yes or no"
+                echo "Please, reply yes or no"
 
-if yes "classical":
+var questionmark = "!"
+if yes("classical", questionmark):
     echo "Me too"
 else:
     echo "Can't bel"
