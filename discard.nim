40f2a59afae95fc20da60de4f05625e4090f02f6
Arialdo Martini <arialdomartini@gmail.com>
2015-04-22 06:51:55 +0200

    nim has a c++ like procedure overloading

--- discard.nim
+++ discard.nim
@@ -11,5 +11,10 @@ let a = return_some_value(1, 2)
 proc print_its_own_parameters(first: string, second: string) =
   echo "first = ", first, "; second = ", second
 
+proc print_its_own_parameters(first: int, second: int) =
+  echo "first = ", first, "; second = ", second
+
 print_its_own_parameters("one", "two")
 print_its_own_parameters(second = "two", first ="one")
+
+print_its_own_parameters(first=10, second=20)
